faa845d30e7ac45adfa337552ab6681d0e1a5037
Ilzadzul <laifaplace14@gmail.com>
2024-04-17 19:13:53 +0700

    K6RS-4

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-
+use App\Http\Controllers\SuperAdminController;
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -16,3 +16,12 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+
+
+Route::get('/cari-penduduk-pelatihan-x', [SuperAdminController::class, 'cariPendudukPelatihanX']);
+// app/Http/Controllers/SuperAdminController.php
+
+
+
+
