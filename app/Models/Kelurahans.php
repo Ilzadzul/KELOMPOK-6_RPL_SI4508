f7ad53018ce06659b2f9519586394511ad5ce472
salmalya <almalyas@student.telkomuniversity.ac.id>
2024-06-01 19:18:26 +0700

    Laravel dusk testing [issue resolution wilayah delete]

--- app/Models/Kelurahans.php
+++ app/Models/Kelurahans.php
@@ -20,4 +20,12 @@ class Kelurahans extends Model
     {
         return $this->hasMany(Penduduk::class, 'nama_kelurahan', 'name');
     }
+    protected static function boot()
+    {
+        parent::boot();
+
+        static::deleting(function ($kelurahan) {
+            $kelurahan->penduduk()->delete();
+        });
+    }
 }
